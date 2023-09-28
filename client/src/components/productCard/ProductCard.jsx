7455978392b8e67fe803847eee62b2cb6fc62b32
DimaHohol <terokhin.d@gmail.com>
2023-09-29 00:30:14 +0300

    Создал компонент "Продуктовая карточка".
    Карточка в ProductCard.jsx
    Стиль в ProductCard.scss
    
    Добавил файл _catalog.scss  где прописал стили для каталога в котором показываються карточки.

--- client/src/components/productCard/ProductCard.jsx
+++ client/src/components/productCard/ProductCard.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import "./ProductCard.scss";
+
 function ProductCard() {
-  return <div />;
+  return (
+    <div className="catalog">
+      <div className="jewelry-card">
+        <div className="jewelry-card-image">
+          <div className="jewlry-sale">
+            <h2 className="jewlry-sale-info">-20%</h2>
+          </div>
+          <img
+            className="jewelry-card-pic"
+            src={"/assets/images/jewelry.png"}
+            alt="jewelry-pic"
+          />
+        </div>
+        <div className="jewelry-card-info">
+          <h2 className="jewelry-card-title">Jewelry</h2>
+          <p className="jewelry-card-price">$20,00</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import ProductCard from "../components/productCard/ProductCard";
+import "../styles/style.scss";
 
 function Catalog() {
   return (
-    <>
-      <h2>Catalog</h2>
+    <div className="main-catalog">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
       <Outlet />
-    </>
+    </div>
   );
 }
 export default Catalog;
