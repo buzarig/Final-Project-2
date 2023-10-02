5d6ebc3d98d3e3fe541ba5926204f0db31c4ec2f
R0st1k8787 <maks.rastik.2405@gmail.com>
2023-10-02 13:55:22 +0300

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CarouselComponent from "../components/carousel/Carousel";
 import "../styles/_home.scss";
+import ProductCard from "../components/productCard/ProductCard";
 
 function Home() {
   return (
@@ -11,7 +12,17 @@ function Home() {
       </main>
       <div className="flex-shop">
         <h2 className="title-shop">Shop The Latest</h2>
-        <Link to="/catalog">View all</Link>
+        <Link to="/catalog" className="shop-link">
+          View all
+        </Link>
+      </div>
+      <div className="cards-container">
+        <ProductCard className="product-card" />
+        <ProductCard className="product-card" />
+        <ProductCard className="product-card" />
+        <ProductCard className="product-card" />
+        <ProductCard className="product-card" />
+        <ProductCard className="product-card" />
       </div>
     </div>
   );
