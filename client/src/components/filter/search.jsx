31e83426eb6f7a47c0f57ce013b40ba17706085f
Mykola Batrak <kolyaun@live.com>
2023-10-04 11:59:45 +0300

    style transfer

--- client/src/components/filter/search.jsx
+++ client/src/components/filter/search.jsx
@@ -1,22 +1,46 @@
 import React, { useState } from "react";
 import img from "../../assets/images/filter/search.png";
-import "./_search.scss";
+import Box from "@mui/material/Box";
+
+const formStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  borderBottom: "1px solid #03141215",
+  marginBottom: "39px",
+  "&:hover": {
+    borderBottom: "1px solid #031412"
+  }
+};
+
+const inputStyle = {
+  width: "100%",
+  fontFamily: "DM Sans",
+  fontSize: "16px",
+  fontWeight: "400",
+  color: "#031412",
+  marginBottom: "10px"
+};
 
 const Search = () => {
-    
-    const [setValue] = useState();
+  const [setValue] = useState();
 
-    return (
-      <form className="search__form">
-        <input
-          className="search__input"
-          type="text"
-          placeholder="Search..."
-          onChange={(event) => setValue(event.target.value)}
-        />
-        <img className="search__icon" src={img} alt="search" />
-      </form>
-    );
-  };
-  
-  export default Search;
+  return (
+    <Box sx={formStyle}>
+      <input
+        style={inputStyle}
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <img
+        style={{ height: "17px", width: "17px", marginBottom: "10px" }}
+        src={img}
+        alt="search"
+      />
+    </Box>
+  );
+};
+
+export default Search;
