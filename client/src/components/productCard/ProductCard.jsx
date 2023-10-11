3225921758f993e55bccd96806cc15e53ca5d0e4
DimaHohol <terokhin.d@gmail.com>
2023-10-11 23:26:04 +0300

    I rewrote the card taking into account the props. I also rewrote the style file.

--- client/src/components/productCard/ProductCard.jsx
+++ client/src/components/productCard/ProductCard.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 import "./ProductCard.scss";
 
-function ProductCard() {
+function ProductCard(props) {
+  const { discount, title, price, imageUrl } = props;
+
   return (
     <div className="catalog">
       <div className="jewelry-card">
         <div className="jewelry-card-image">
-          <div className="jewlry-sale">
-            <h2 className="jewlry-sale-info">-20%</h2>
-          </div>
-          <img
-            className="jewelry-card-pic"
-            src={"/assets/images/jewelry.png"}
-            alt="jewelry-pic"
-          />
+          {discount && (
+            <div className="jewlry-sale">
+              <h2 className="jewlry-sale-info">-{discount}%</h2>
+            </div>
+          )}
+          <img className="jewelry-card-pic" src={imageUrl} alt="jewelry-pic" />
         </div>
         <div className="jewelry-card-info">
-          <h2 className="jewelry-card-title">Jewelry</h2>
-          <p className="jewelry-card-price">$20,00</p>
+          <h2 className="jewelry-card-title">{title}</h2>
+          <p className="jewelry-card-price">${price}</p>
         </div>
       </div>
     </div>
