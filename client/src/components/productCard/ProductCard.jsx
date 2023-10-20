9ffdb0b1688ca6faff51366462c6400d5fd25408
DimaHohol <terokhin.d@gmail.com>
2023-10-20 13:02:35 +0300

    Отредактировал шрифты. Проверил стили.

--- client/src/components/productCard/ProductCard.jsx
+++ client/src/components/productCard/ProductCard.jsx
@@ -6,20 +6,20 @@ function ProductCard(props) {
   const { discount, title, price, imageUrl } = props;
 
   return (
-      <div className="jewelry-card">
-        <div className="jewelry-card-image">
-          {discount && (
-            <div className="jewlry-sale">
-              <h2 className="jewlry-sale-info">-{discount}%</h2>
-            </div>
-          )}
-          <img className="jewelry-card-pic" src={imageUrl} alt="jewelry-pic" />
-        </div>
-        <div className="jewelry-card-info">
-          <h2 className="jewelry-card-title">{title}</h2>
-          <p className="jewelry-card-price">${price}</p>
-        </div>
+    <div className="jewelry-card">
+      <div className="jewelry-card-image">
+        {discount && (
+          <div className="jewlry-sale">
+            <h2 className="jewlry-sale-info">-{discount}%</h2>
+          </div>
+        )}
+        <img className="jewelry-card-pic" src={imageUrl} alt="jewelry-pic" />
       </div>
+      <div className="jewelry-card-info">
+        <h2 className="jewelry-card-title">{title}</h2>
+        <p className="jewelry-card-price">${price}</p>
+      </div>
+    </div>
   );
 }
 
