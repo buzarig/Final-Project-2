61e4eaac545c2342c86e772e5c461cbd5ada06b2
Kolya Kulchytskyy <virgocalf@gmail.com>
2023-10-28 15:04:13 +0300

    work with back

--- client/src/components/productCard/ProductCard.jsx
+++ client/src/components/productCard/ProductCard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable react/require-default-props */
+
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
