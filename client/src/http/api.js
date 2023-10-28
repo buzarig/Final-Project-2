516c987de5280d7e9f52c049253d6e08e78552b1
Kolya Kulchytskyy <virgocalf@gmail.com>
2023-10-28 15:10:00 +0300

    added env

--- client/src/http/api.js
+++ client/src/http/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   mode: "no-cors",
-  baseURL: "https://final-project-backend-snpn.onrender.com/api"
+  baseURL: "http://localhost:4000/api"
 });
 
 api.interceptors.request.use((config) => {
