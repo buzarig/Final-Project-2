f268219b90070831078a3b897ae4dcfb594fe377
Kolya Kulchytskyy <virgocalf@gmail.com>
2023-11-13 13:06:21 +0200

    orders in comment

--- client/src/pages/navCabinet/Orders.jsx
+++ client/src/pages/navCabinet/Orders.jsx
@@ -1,73 +1,86 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import api from "../../http/api";
+// import React, { useState, useEffect } from "react";
+// import { useSelector } from "react-redux";
+// import { Link } from "react-router-dom";
+// import api from "../../http/api";
 
-function Orders() {
-  const [userName, setUserName] = useState("");
-  const [orders, setOrders] = useState([]);
+// function Orders() {
+//   // const [userName, setUserName] = useState("");
+//   // const [orders, setOrders] = useState([]);
+//   // const [surname, setSurname] = useState(""); // Добавлено
+//   // const [userEmail, setUserEmail] = useState(""); // Добавлено
+//   // const [userLogin, setUserLogin] = useState(""); // Добавлено
+//   // const [currentPassword, setCurrentPassword] = useState(""); // Добавлено
 
-  const token = useSelector((state) => state.token.accessToken);
+//   // const token = useSelector((state) => state.token.accessToken);
 
-  async function fetchData() {
-    try {
-      const response = await api.get("/customers/customer", {
-        headers: {
-          Authorization: token
-        }
-      });
+//   // async function fetchData() {
+//   //   try {
+//   //     const response = await api.get("/customers/customer", {
+//   //       headers: {
+//   //         Authorization: token
+//   //       }
+//   //     });
 
-      if (response.status === 200) {
-        const dataResponse = response.data;
-        setUserName(dataResponse.firstName);
-        setSurname(dataResponse.lastName);
-        setUserEmail(dataResponse.email);
-        setUserLogin(dataResponse.login);
-        setCurrentPassword("");
-      } else {
-        console.log("Error when retrieving user data:", response.status);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
+//   //     if (response.status === 200) {
+//   //       const dataResponse = response.data;
+//   //       setUserName(dataResponse.firstName);
+//   //       setSurname(dataResponse.lastName);
+//   //       setUserEmail(dataResponse.email);
+//   //       setUserLogin(dataResponse.login);
+//   //       setCurrentPassword("");
+//   //     } else {
+//   //       console.log("Error when retrieving user data:", response.status);
+//   //     }
+//   //   } catch (error) {
+//   //     console.error("Error:", error);
+//   //   }
+//   // }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   // async function fetchOrders() {
+//   //   try {
+//   //     // Реализуйте логику получения заказов из API
+//   //     // Например: const ordersResponse = await api.get("/orders");
+//   //     // Затем установите заказы в состояние: setOrders(ordersResponse.data);
+//   //   } catch (error) {
+//   //     console.error("Error fetching orders:", error);
+//   //   }
+//   // }
 
-  useEffect(() => {
-    fetchOrders();
-  }, []);
+//   // useEffect(() => {
+//   //   fetchData();
+//   // }, [fetchData]);
 
-  return (
-    <>
-      {orders.length > 0 ? (
-        <ul className="orders-list">
-          {orders.map((order) => (
-            <li key={order.id}>
-              {/* Вставить ключи от заказа */}
-              <p className="orders-text">ORDER NUMBER</p>
-              <p className="orders-text">DATE</p>
-              <p className="orders-text">STATUS</p>
-              <p className="orders-text">TOTAL</p>
-              <p className="orders-text">ACTIONS</p>
-              <p className="orders-link">
-                <Link>View Order</Link>
-              </p>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div className="orders-block">
-          <p className="orders-text">No order has been made yet.</p>
-          <p className="orders-link">
-            <Link to="/catalog">Browse Product</Link>
-          </p>
-        </div>
-      )}
-    </>
-  );
-}
+//   // useEffect(() => {
+//   //   fetchOrders();
+//   // }, [token]);
 
-export default Orders;
+//   return (
+//     <div>
+//       {orders.length > 0 ? (
+//         <ul className="orders-list">
+//           {orders.map((order) => (
+//             <li key={order.id}>
+//               <p className="orders-text">ORDER NUMBER: {order.orderNumber}</p>
+//               <p className="orders-text">DATE: {order.date}</p>
+//               <p className="orders-text">STATUS: {order.status}</p>
+//               <p className="orders-text">TOTAL: {order.total}</p>
+//               <p className="orders-text">ACTIONS</p>
+//               <p className="orders-link">
+//                 <Link to={`/order/${order.id}`}>View Order</Link>
+//               </p>
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <div className="orders-block">
+//           <p className="orders-text">No order has been made yet.</p>
+//           <p className="orders-link">
+//             <Link to="/catalog">Browse Product</Link>
+//           </p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Orders;
