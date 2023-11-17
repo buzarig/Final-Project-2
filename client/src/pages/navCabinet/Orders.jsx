c8f508b0a1b699296fac1d960d39ac80ca115ae6
R0st1k8787 <maks.rastik.2405@gmail.com>
2023-11-17 11:16:54 +0200

    add styles for orders

--- client/src/pages/navCabinet/Orders.jsx
+++ client/src/pages/navCabinet/Orders.jsx
@@ -1,14 +1,70 @@
-import React from "react";
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-console */
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import api from "../../http/api";
 
 function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  const token = useSelector((state) => state.token.accessToken);
+
+  async function fetchOrders() {
+    try {
+      const response = await api.get("/orders", {
+        headers: {
+          Authorization: token
+        }
+      });
+
+      if (response.status === 200) {
+        setOrders(response.data);
+      } else {
+        console.log("Error when retrieving user data:", response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
   return (
-    <div className="orders-block">
-      <p className="orders-text">No order has been made yet.</p>
-      <p className="orders-link">
-        <Link to="/catalog">Browse Product</Link>
-      </p>
-    </div>
+    <>
+      {orders.length > 0 ? (
+        <ul className="orders-list">
+          <li className="orders-header">
+            <p className="orders-text">ORDER NUMBER</p>
+            <p className="orders-text">DATE</p>
+            <p className="orders-text">TOTAL</p>
+            <p className="orders-text">ACTIONS</p>
+          </li>
+          {orders.map((order) => (
+            <li className="orders-list" key={order.id}>
+              <p className="orders-text">{order.orderNo}</p>
+              <p className="orders-text">{order.date}</p>
+              <p className="orders-text">$ {order.totalSum}</p>
+              <p className="orders-text">
+                <Link to={`/orders/${order.id}`} className="orders-link">
+                  View Order
+                </Link>
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="orders-block">
+          <p className="orders-text">No order has been made yet.</p>
+          <p className="orders-link">
+            <Link to="/catalog">Browse Product</Link>
+          </p>
+        </div>
+      )}
+    </>
   );
 }
 
